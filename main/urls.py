14a57c987bbe76083c4ce3d93cfa56166c93735a
rubychaudhary0 <rubychaudhary743@gmail.com>
2023-12-29 14:14:10 +0545

    password reset

--- main/urls.py
+++ main/urls.py
@@ -40,21 +40,21 @@ urlpatterns = [
 
 
     # change password
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='main/registration/password_change_done.html'), 
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='main/authentication/password_change_done.html'), 
         name='password_change_done'),
 
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='main/registration/password_change.html', success_url = reverse_lazy("password_change_done")), 
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='main/authentication/password_change.html', success_url = reverse_lazy("password_change_done")), 
         name='password_change'),
 
     #Forgot password
-    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name = "main/registration/password_reset_form.html", success_url = reverse_lazy("password_reset_complete")), 
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name = "main/authentication/password_reset_form.html", success_url = reverse_lazy("password_reset_complete")), 
     name="password_reset_confirm"),  # 3
-    path('reset_password/',auth_views.PasswordResetView.as_view(template_name = "main/registration/password_reset.html", success_url = reverse_lazy("password_reset_done"), email_template_name = 'main/registration/forgot_password_email.html'), 
+    path('reset_password/',auth_views.PasswordResetView.as_view(template_name = "main/authentication/password_reset.html", success_url = reverse_lazy("password_reset_done"), email_template_name = 'main/authentication/forgot_password_email.html'), 
     name="reset_password"),     # 1
-    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name = "main/registration/password_reset_sent.html"), 
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name = "main/authentication/password_reset_sent.html"), 
     name="password_reset_done"),    # 2
     
-    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name = "main/registration/password_reset_done.html"), 
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name = "main/authentication/password_reset_done.html"), 
     name="password_reset_complete"),   # 4
     
 
--- main/views.py
+++ main/views.py
@@ -77,7 +77,7 @@ class RegisterView(CreateView):
             user.save()
             current_site = get_current_site(request)     
             mail_subject = 'Activate your account.'
-            message = render_to_string('main/registration/acc_active_email.html', {
+            message = render_to_string('main/authentication/acc_active_email.html', {
                 'user': user,
                 'domain': current_site.domain,
                 'uid':urlsafe_base64_encode(force_bytes(user.pk)),
